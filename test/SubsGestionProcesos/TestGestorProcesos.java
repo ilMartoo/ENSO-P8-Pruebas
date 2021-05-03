6936f08dfc663c7eee3401e91cdf7b1ad0774fa1
pablito2199 <63094934+pablito2199@users.noreply.github.com>
2021-05-03 12:31:08 +0200

    Update TestGestorProcesos.java

--- test/SubsGestionProcesos/TestGestorProcesos.java
+++ test/SubsGestionProcesos/TestGestorProcesos.java
@@ -1,5 +1,32 @@
 package SubsGestionProcesos;
 
-public class TestGestorProcesos {
+import static org.junit.jupiter.api.Assertions.fail;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TestGestorProcesos {
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
 }
